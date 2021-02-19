be91522cf5bd554fc023bf5b8ed93b3bbe469665
ecbob <chestercervantes13@hotmail.com>
2021-02-18 23:25:07 -0800

    - show champion imgs in poll

--- client/src/components/Poll.jsx
+++ client/src/components/Poll.jsx
@@ -1,28 +1,13 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import {Pie} from 'react-chartjs-2';
 
-import Champions from "./Champions";
 import {vote} from "../store/actions";
+import {Col, Container, Row} from "react-grid-system";
 
 // Needs to change if poll has more than two options
 const COLORS = ['#6699cc', '#CC4C4C'];
 
-function formatTitle(championsBlue, championsRed) {
-    let text = "";
-    let i;
-    if (championsBlue && championsRed) {
-        for (i = 0; i < championsBlue.length - 1; i++) {
-            text += championsBlue[i] + ", ";
-        }
-        text += championsBlue[i] + " || ";
-        for (i = 0; i < championsRed.length - 1; i++) {
-            text += championsRed[i] + ", ";
-        }
-        text += championsRed[i];
-    }
-    return text
-}
 
 function Poll(props) {
     const {poll, vote, auth} = props;
@@ -52,19 +37,141 @@ function Poll(props) {
     // Check if user already voted
     const hasVoted = () => poll.voted.indexOf(auth.user.id) >= 0;
 
+    useEffect(() => {
+        const replaceImage = (champion, role) => {
+            let src = "http://ddragon.leagueoflegends.com/cdn/11.3.1/img/champion/" + champion + ".png";
+            let el = document.getElementById(role);
+            console.log(el);
+            if (el) {
+                let elImg = el.firstElementChild;
+                elImg.src = src;
+                elImg.alt = role;
+            }
+        };
+
+        const replaceImages = () => {
+            if (poll.topBlue && poll.jungleBlue && poll.midBlue && poll.botBlue && poll.supportBlue && poll.topRed && poll.jungleRed && poll.midRed && poll.botRed && poll.supportRed) {
+                replaceImage(poll.topBlue, "topBlue");
+                replaceImage(poll.jungleBlue, "jungleBlue");
+                replaceImage(poll.midBlue, "midBlue");
+                replaceImage(poll.botBlue, "botBlue");
+                replaceImage(poll.supportBlue, "supportBlue");
+                replaceImage(poll.topRed, "topRed");
+                replaceImage(poll.jungleRed, "jungleRed");
+                replaceImage(poll.midRed, "midRed");
+                replaceImage(poll.botRed, "botRed");
+                replaceImage(poll.supportRed, "supportRed");
+            }
+        };
+        console.log("works")
+
+        replaceImages();
+    }, []);
+
+    const make = () => {
+
+    }
+
     return (
         <Fragment>
-            <h3 className="poll-title">{formatTitle(poll.championsBlue, poll.championsRed)}</h3>
+            {/*<h3 className="poll-title">{formatTitle(poll.topBlue, poll.jungleBlue, poll.midBlue,*/}
+            {/*    poll.botBlue, poll.supportBlue, poll.topRed, poll.jungleRed, poll.midRed, poll.botRed, poll.supportRed)}</h3>*/}
+            <h3 className="poll-title">Which team composition is better?
+            </h3>
+
+            <br/>
+            <hr/>
+            <br/>
+
             <div className="buttons_center">{answers}</div>
             <div className="buttons_center">
                 <button className="button" onClick={() => setShowGraph(true)}>Show graph without voting</button>
             </div>
+
+            <br/>
+            <hr/>
+            <br/>
+
+            <Container>
+                <Row>
+                    <Col sm={6}>
+                        <div className="blue-side">
+                            <div className="pick-container" id="topBlue">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.topBlue}</p>
+                            </div>
+                            <div className="pick-container" id="jungleBlue">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.jungleBlue}</p>
+                            </div>
+                            <div className="pick-container" id="midBlue">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.midBlue}</p>
+                            </div>
+                            <div className="pick-container" id="botBlue">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.botBlue}</p>
+                            </div>
+                            <div className="pick-container" id="supportBlue">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.supportBlue}</p>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col sm={6}>
+                        <div className="red-side">
+                            <div className="pick-container" id="topRed">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.topRed}</p>
+                            </div>
+                            <div className="pick-container" id="jungleRed">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.jungleRed}</p>
+                            </div>
+                            <div className="pick-container" id="midRed">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.midRed}</p>
+                            </div>
+                            <div className="pick-container" id="botRed">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.botRed}</p>
+                            </div>
+                            <div className="pick-container" id="supportRed">
+                                <img
+                                    src="http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png"
+                                    alt="none"/>
+                                <p>&nbsp;{poll.supportRed}</p>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <br/>
+            <hr/>
             {poll.options && (showGraph || hasVoted()) && <Pie data={data}/>}
+            <br/>
         </Fragment>
     )
 
 }
-
 
 
 export default connect(store => ({
